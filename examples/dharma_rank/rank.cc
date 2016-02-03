1893bea1cb736d7d2ade448828e545df08f2e1f2
Francesco Rizzi <fnrizzi@sandia.gov>
2016-02-03 10:12:28 -0800

    examples: adding hello world dharma and revised the dharma rank example

--- examples/dharma_rank/rank.cc
+++ examples/dharma_rank/rank.cc
@@ -8,11 +8,14 @@ using namespace dharma_runtime;
 
 int main(int argc, char** argv)
 {
+	// intializes dharma environment (similar to MPI_Init)
   dharma_init(argc, argv);
 
+  // get my rank and the size of the ``dharma world''
   size_t me = dharma_spmd_rank();
   size_t n_spmd = dharma_spmd_size();
 
+  // print to standard output my rank
   std::cout << "I am dharma rank = " << me << std::endl;
 
   dharma_finalize();
