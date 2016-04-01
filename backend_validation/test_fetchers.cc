001e2b94ce2e8a4d7702bbd74218a01265ae8e0d
Nicole Lemaster Slattengren <nlslatt@sandia.gov>
2016-04-01 11:42:06 -0700

    Addition backend validation test.

--- backend_validation/test_fetchers.cc
+++ backend_validation/test_fetchers.cc
@@ -910,3 +910,67 @@ TEST_F(TestFetchers, satisfy_fetcher_reg2_then_pub2) {
   detail::backend_runtime->release_handle(h_1.get());
 }
 
+// publish with n_fetchers=0
+TEST_F(TestFetchers, publish_no_fetchers) {
+  using namespace ::testing;
+
+  auto h_0 = make_handle<int, true, true>(MockDependencyHandle::version_t(), "the_key");
+  auto next_version = h_0->get_version();
+  ++next_version;
+  auto h_1 = make_handle<int, true, false>(next_version, "the_key");
+
+  EXPECT_CALL(*h_0.get(), satisfy_with_data_block(_))
+    .Times(Exactly(1));
+  EXPECT_CALL(*h_0.get(), get_data_block())
+    .Times(AtLeast(2));  // when running write-only task and when releasing
+
+  EXPECT_CALL(*h_1.get(), satisfy_with_data_block(_))
+    .Times(Exactly(1));
+  EXPECT_CALL(*h_1.get(), get_data_block())
+    .Times(AtLeast(1));
+  EXPECT_CALL(*h_1.get(), allow_writes())
+    .Times(AtMost(1));
+
+  auto user_ver = make_key("alpha");
+
+  detail::backend_runtime->register_handle(h_0.get());
+  detail::backend_runtime->register_handle(h_1.get());
+
+  detail::backend_runtime->release_read_only_usage(h_0.get());
+
+  int value = 42;
+
+  // Increment the version depth of h_0 in preparation for write-only capture
+  h_0.get()->get_version_return.push_subversion();
+  register_write_only_capture(h_0.get(), [&,value]{
+    ASSERT_TRUE(h_0->is_satisfied());
+    ASSERT_TRUE(h_0->is_writable());
+    abstract::backend::DataBlock* data_block = h_0->get_data_block();
+    ASSERT_THAT(data_block, NotNull());
+    void* data = data_block->get_data();
+    ASSERT_THAT(data, NotNull());
+    memcpy(data, &value, sizeof(int));
+    detail::backend_runtime->handle_done_with_version_depth(h_0.get());
+    detail::backend_runtime->release_handle(h_0.get());
+  });
+
+  // publish with zero fetchers (should not cause a hang or other undesirable
+  // behavior that is not directly measured)
+  detail::backend_runtime->publish_handle(h_1.get(), user_ver, 0);
+
+  detail::backend_runtime->finalize();
+  backend_finalized = true;
+
+  ASSERT_TRUE(h_1->is_satisfied());
+  {
+    abstract::backend::DataBlock* data_block = h_1->get_data_block();
+    ASSERT_THAT(data_block, NotNull());
+    void* data = data_block->get_data();
+    ASSERT_THAT(data, NotNull());
+    ASSERT_THAT(*((int*)data), Eq(value));
+  }
+
+  detail::backend_runtime->release_read_only_usage(h_1.get());
+  detail::backend_runtime->release_handle(h_1.get());
+}
+
