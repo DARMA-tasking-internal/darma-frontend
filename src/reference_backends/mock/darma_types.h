eade7ecf56b0d72cd9bd61e7cf11697d1632dae1
Nicole Lemaster Slattengren <nlslatt@sandia.gov>
2016-04-06 12:01:41 -0700

    Removing mock and stl_threads backends for 0.2.

--- src/reference_backends/mock/darma_types.h
+++
@@ -1,6 +0,0 @@
-#define DARMA_BACKEND_SPMD_NAME_PREFIX "spmd"
-
-#include <darma/interface/defaults/key.h>
-#include <darma/interface/defaults/version.h>
-#include <darma/interface/defaults/pointers.h>
-
--- src/reference_backends/stl_threads/darma_types.h
+++
@@ -1,8 +0,0 @@
-#define DARMA_BACKEND_SPMD_NAME_PREFIX "spmd"
-
-#define DARMA_STL_THREADS_BACKEND 1
-
-#include <darma/interface/defaults/key.h>
-#include <darma/interface/defaults/version.h>
-#include <darma/interface/defaults/pointers.h>
-
