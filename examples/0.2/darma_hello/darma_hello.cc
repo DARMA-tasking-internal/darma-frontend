88be8aa5ca2ba1a169c8ede5b2923637f6e7bf87
Francesco Rizzi <fnrizzi@sandia.gov>
2016-04-28 14:59:07 -0700

    Two main changes: (1) reordered examples folder so that there is a 0.2 folder containing all examples running under 0.2 spec. (Based on Janine feedback) (2) updating the examples chapter in the documentation

--- examples/0.2/darma_hello/darma_hello.cc
+++ examples/0.2/darma_hello/darma_hello.cc
@@ -1,25 +1,23 @@
-
 #include <darma.h>
-
 int darma_main(int argc, char** argv)
 {
   using namespace darma_runtime;
 
   darma_init(argc, argv);
-
   size_t me = darma_spmd_rank();
   size_t n_ranks = darma_spmd_size();
 
-  AccessHandle<std::string> greetingMessage = initial_access<std::string>("myName", me);
+  auto greeting = initial_access<std::string>("myName", me);
 
   create_work([=]
   {
-    greetingMessage.set_value("hello world!");
+    greeting.set_value("hello world!");
   });
 
   create_work([=]
   {
-    std::cout << "DARMA rank " << me << " says " << greetingMessage.get_value() << std::endl;
+    std::cout << "DARMA rank " << me 
+              << " says: " << greeting.get_value() << std::endl;
   });
 
   darma_finalize();
