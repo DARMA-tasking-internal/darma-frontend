e26586b9118c52b750fd65d46be999b5384a46e7
Nicole Lemaster Slattengren <nlslatt@sandia.gov>
2016-05-13 10:08:55 -0700

    Added death tests to backend validation.

---
+++ backend_validation/test_death.cc
@@ -0,0 +1,167 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                          test_death.cc
+//                         dharma_new
+//              Copyright (C) 2016 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact David S. Hollman (dshollm@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#ifdef TEST_BACKEND_INCLUDE
+#  include TEST_BACKEND_INCLUDE
+#endif
+
+#include "mock_frontend.h"
+#include "helpers.h"
+
+using namespace darma_runtime;
+
+namespace {
+
+class DARMADeathTest
+  : public ::testing::Test
+{
+  protected:
+
+    static void
+    init_backend() {
+      int argc = 1;
+      char **argv = new char*[1];
+      argv[0] = new char[256];
+      sprintf(argv[0], "<mock frontend test>");
+
+      std::unique_ptr<typename abstract::backend::runtime_t::task_t>
+          top_level_task = std::make_unique<
+          ::testing::NiceMock<mock_frontend::MockTask>>();
+
+      abstract::backend::darma_backend_initialize(
+        argc, argv, detail::backend_runtime,
+        std::move(top_level_task)
+      );
+    }
+
+    template <typename Version, typename... KeyParts>
+    static std::shared_ptr<NiceMock<MockDependencyHandle>>
+    make_handle(Version v, KeyParts&&... kp) {
+      typedef NiceMock<MockSerializationManager> ser_man_t;
+      typedef NiceMock<MockDependencyHandle> handle_t;
+      auto ser_man = new ser_man_t;
+      EXPECT_CALL(*ser_man, get_metadata_size(IsNull()))
+        .Times(AtLeast(1))
+        .WillRepeatedly(Return(sizeof(int)));
+      auto new_handle = std::shared_ptr<handle_t>(
+        new handle_t(),
+        [=](handle_t* to_delete){
+          delete to_delete;
+          delete ser_man;
+        }
+      );
+      new_handle->get_key_return = detail::key_traits<MockDependencyHandle::key_t>::maker()(
+          std::forward<KeyParts>(kp)...);
+      new_handle->get_version_return = v;
+      EXPECT_CALL(*new_handle, get_serialization_manager())
+        .Times(AnyNumber())
+        .WillRepeatedly(Return(ser_man));
+      return new_handle;
+    }
+
+    virtual ~DARMADeathTest() { }
+};
+
+} // end anonymous namespace
+
+#if defined(DEBUG) || !defined(NDEBUG)
+
+TEST_F(DARMADeathTest, release_read_only_usage_before_register_handle) {
+  using namespace ::testing;
+
+  EXPECT_DEATH(
+    {
+      init_backend();
+      auto h_0 = this->make_handle(
+          MockDependencyHandle::version_t(), "the_key");
+      detail::backend_runtime->release_read_only_usage(h_0.get());
+    },
+    "release_read_only_usage"
+  );
+}
+
+TEST_F(DARMADeathTest, release_read_only_usage_after_release_handle) {
+  using namespace ::testing;
+
+  EXPECT_DEATH(
+    {
+      init_backend();
+      auto h_0 = this->make_handle(
+          MockDependencyHandle::version_t(), "the_key");
+
+      detail::backend_runtime->register_handle(h_0.get());
+      detail::backend_runtime->release_read_only_usage(h_0.get());
+      detail::backend_runtime->release_handle(h_0.get());
+
+      detail::backend_runtime->release_read_only_usage(h_0.get());
+    },
+    "release_read_only_usage"
+  );
+}
+
+TEST_F(DARMADeathTest, publish_handle_after_release_handle) {
+  using namespace ::testing;
+
+  EXPECT_DEATH(
+    {
+      init_backend();
+      auto h_0 = this->make_handle(
+          MockDependencyHandle::version_t(), "the_key");
+
+      detail::backend_runtime->register_handle(h_0.get());
+      detail::backend_runtime->release_read_only_usage(h_0.get());
+      detail::backend_runtime->release_handle(h_0.get());
+
+      auto user_ver = make_key("alpha");
+      detail::backend_runtime->publish_handle(h_0.get(), user_ver);
+    },
+    "publish_handle"
+  );
+}
+
+#endif
