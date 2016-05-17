6636b4e24a10aca23d198e48049647955874f27e
Janine Bennett <jcbenne@sandia.gov>
2016-05-16 17:49:49 -0700

    a lot of edits to the spec doc.  These should have been done in a more incremental set of commits, but changes got away from me...

--- docs/spec/build-spec.sh
+++ docs/spec/build-spec.sh
@@ -7,6 +7,8 @@ makeglossaries sandReportSpec
 pdflatex sandReportSpec                                                          
 makeglossaries sandReportSpec
 pdflatex sandReportSpec                                                          
+makeglossaries sandReportSpec
+pdflatex sandReportSpec                                                          
 bibtex sandReportSpec
 pdflatex sandReportSpec
 pdflatex sandReportSpec
