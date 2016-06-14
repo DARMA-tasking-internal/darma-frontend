70d0709f5c40fa048ea5beaea03bb740bb1b1199
David Hollman <dshollm@sandia.gov>
2016-06-14 10:28:17 -0700

    added default_construct() to SerializationManager as a hold-over until we get something more robust working

--- backend_validation/mock_frontend.h
+++ backend_validation/mock_frontend.h
@@ -68,6 +68,7 @@ class MockSerializationManager
     MOCK_CONST_METHOD1(get_packed_data_size, size_t(const void* const));
     MOCK_CONST_METHOD2(pack_data, void(const void* const, void* const));
     MOCK_CONST_METHOD2(unpack_data, void(void* const, const void* const));
+    MOCK_CONST_METHOD1(default_construct, void(void*));
 };
 
 //class MockDependencyHandle
