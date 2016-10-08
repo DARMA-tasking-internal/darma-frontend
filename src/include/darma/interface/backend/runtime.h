4cdb55b37db6154ba91d048126ccaf7cc1962b81
Jonathan Lifflander <jliffla@sandia.gov>
2016-10-07 20:03:55 -0700

    cleanup types in the backend

--- src/include/darma/interface/backend/runtime.h
+++ src/include/darma/interface/backend/runtime.h
@@ -98,6 +98,10 @@ class Runtime {
       std::unique_ptr<generic_to_compact_index_mapping_t>;
     using top_level_task_t = abstract::frontend::TopLevelTask<types::concrete_task_t>;
     using top_level_task_unique_ptr = std::unique_ptr<top_level_task_t>;
+    using use_t = frontend::Use;
+    using pub_details_t = darma_runtime::abstract::frontend::PublicationDetails;
+    using collective_details_t = darma_runtime::abstract::frontend::CollectiveDetails;
+    using memory_details_t = abstract::frontend::MemoryRequirementDetails;
 
     //==========================================================================
 
