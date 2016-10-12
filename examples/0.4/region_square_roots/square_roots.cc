cc8f987c4ec08d9e71656dd09f2a0a40fe8ad1c1
David Hollman <dshollm@sandia.gov>
2016-10-12 15:19:20 -0700

    added square roots example for load balance testing

--- examples/0.4/region_square_roots/square_roots.cc
+++ examples/0.4/region_square_roots/square_roots.cc
@@ -41,3 +41,67 @@
 // ************************************************************************
 //@HEADER
 */
+
+#include <random>
+
+#include <darma.h>
+#include <darma/impl/array/index_range.h>
+
+using namespace darma_runtime;
+
+static void escape(void* p) {
+  asm volatile("" : : "g"(p) : "memory");
+}
+
+static void clobber() {
+  asm volatile("" : : : "memory");
+}
+
+
+struct SquareRoots {
+  void operator()(
+    ConcurrentRegionContext<Range1D<int>> context,
+    int iteration, int change_interval,
+    int min_per_iter, int max_per_iter
+  ) const {
+
+    clobber();
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    gen.seed(context.index().value * context.index().max_value + iteration/change_interval);
+    std::uniform_int_distribution<> dis(min_per_iter, max_per_iter);
+    std::uniform_real_distribution<> value_dis(1.0, 2.0);
+    int n_sqrts = dis(gen);
+
+    std::vector<double> results;
+    for(int i = 0; i < n_sqrts; ++i) {
+      results.push_back(
+        std::sqrt(value_dis(gen))
+      );
+    }
+
+    escape(results.data());
+
+  }
+};
+
+void darma_main_task(std::vector<std::string> args) {
+  using darma_runtime::keyword_arguments_for_create_concurrent_region::index_range;
+  assert(args.size() == 6);
+
+  size_t const num_ranks = std::atoi(args[1].c_str());
+  size_t const num_iters = std::atoi(args[2].c_str());
+  size_t const change_interval = std::atoi(args[3].c_str());
+  size_t const min_per_iter = std::atoi(args[4].c_str());
+  size_t const max_per_iter = std::atoi(args[5].c_str());
+
+  for(size_t iter = 0; iter < num_iters; ++iter) {
+    create_concurrent_region<SquareRoots>(
+      iter, change_interval, min_per_iter, max_per_iter,
+      index_range = Range1D<int>(num_ranks)
+    );
+  }
+}
+
+DARMA_REGISTER_TOP_LEVEL_FUNCTION(darma_main_task);
