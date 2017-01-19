e2a0f6f1683ce6a377258f118b74ef7ff47fdb65
David Hollman <dshollm@sandia.gov>
2017-01-19 14:46:35 -0700

    about to merge

--- examples/0.4/tutorial/example_5/example_5.cc
+++ examples/0.4/tutorial/example_5/example_5.cc
@@ -13,10 +13,12 @@ struct MyHelloCollection {
     mine.set_value(i.value*42);
     mine.publish();
     auto other = ahc[(i+1) % 2].read_access();
-    std::cout << i.value << ": hello world => ";
-    create_work([=]{
-      std::cout << other.get_value() << std::endl;
-    });
+    if(i.value == 0) {
+      std::cout << i.value << ": hello world => ";
+      create_work([=]{
+        std::cout << other.get_value() << std::endl;
+      });
+    }
   }
 };
 
@@ -25,9 +27,11 @@ darma_main_task(std::vector<std::string> args) {
   using darma::keyword_arguments_for_create_concurrent_work::index_range;
   auto my_range = Range1D<int>(2);
   auto my_hc = initial_access_collection<int>(index_range=my_range);
-  create_concurrent_work<MyHelloCollection>(
-    my_hc, index_range=my_range
-  );
+  for(int i = 0; i < 10000; ++i) {
+    create_concurrent_work<MyHelloCollection>(
+      my_hc, index_range=my_range
+    );
+  }
 }
 
 DARMA_REGISTER_TOP_LEVEL_FUNCTION(darma_main_task);
