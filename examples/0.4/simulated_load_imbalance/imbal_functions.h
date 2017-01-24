cce6c931c0266a552765217479b0797c9a0f3424
David Hollman <dshollm@sandia.gov>
2017-01-23 17:37:00 -0800

    added mpi versions of simulated load imbalance

---
+++ examples/0.4/simulated_load_imbalance/imbal_functions.h
@@ -0,0 +1,105 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                      imbal_functions.h
+//                         DARMA
+//              Copyright (C) 2017 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact David S. Hollman (dshollm@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#ifndef DARMA_IMBAL_FUNCTIONS_H
+#define DARMA_IMBAL_FUNCTIONS_H
+
+//==============================================================================
+// <editor-fold desc="Functions adjusting behavior"> {{{1
+
+double do_workload(double factor) {
+  //std::cout << "do workload: " << factor << std::endl;
+  assert(factor >= 0.0);
+  std::size_t iters = (std::size_t)(factor * 10000);
+  double value = 0;
+  for (std::size_t i = 1; i <= iters; ++i) {
+    value += (double)i * value;
+  }
+  return value;
+}
+
+void do_adjust_workload(
+  double& workload,
+  double adjust_scale,
+  double adjust_amount
+) {
+  workload += adjust_scale * adjust_amount;
+}
+
+double get_adjust_scale(
+  std::size_t adjustment_number,
+  std::size_t total_adjustments
+) {
+  // Simple function, for now:
+  return 1.0 / ((double)adjustment_number + 1.0);
+  //return std::pow(-1.0, adjustment_number) / ((double)adjustment_number + 1.0);
+}
+
+double get_base_load_for_index(
+  std::size_t index,
+  std::size_t n_indices,
+  std::size_t random_seed
+) {
+  //std::cout << "get base load for index: " << index << " " << n_indices << " " << std::endl;
+  std::vector<double> loads;
+  loads.reserve(n_indices);
+  for (std::size_t i = 1; i <= n_indices; ++i) {
+    loads.push_back((double)i);
+  }
+
+  std::random_device rd;
+  std::mt19937_64 g(rd());
+  g.seed(random_seed);
+
+  std::shuffle(loads.begin(), loads.end(), g);
+
+  return loads.at(index);
+
+}
+
+// </editor-fold> end Functions adjusting behavior }}}1
+//==============================================================================
+
+
+#endif //DARMA_IMBAL_FUNCTIONS_H
--- examples/0.4/simulated_load_imbalance/sim_bal_mpi.cc
+++ examples/0.4/simulated_load_imbalance/sim_bal_mpi.cc
@@ -46,48 +46,29 @@
 #include <random>
 #include <cmath>
 
-#include <darma.h>
+#include <mpi.h>
 
 #include "imbal_functions.h"
-
-using namespace darma;
-using namespace darma::keyword_arguments;
-
-
-
 
 //==============================================================================
 // <editor-fold desc="Actual imbalance functor"> {{{1
 
-struct ImbalancedLoad {
+struct BalancedLoad {
 
   void operator()(
-    ConcurrentContext<Index1D<size_t>> ctxt,
+    std::size_t index, std::size_t n_indices,
     std::size_t random_seed, bool adjust_workload,
-    AccessHandleCollection<double, Range1D<size_t>> workloads,
-    AccessHandleCollection<double, Range1D<size_t>> values,
-    double adjust_scale
+    double& workload, double& value, double adjust_scale
   ) const {
 
-    auto my_workload = workloads[ctxt.index()].local_access();
-    auto my_value = values[ctxt.index()].local_access();
-
     if (adjust_workload) {
-      create_work([=] {
-        double base_load = get_base_load_for_index(
-          ctxt.index().value, ctxt.index_count(), random_seed
-        );
-
-        do_adjust_workload(
-          my_workload.get_reference(),
-          adjust_scale, base_load
-        );
+      create_work([=]{
+        double base_load = double(n_indices*(n_indices+1)) / double(2*n_indices);
+        do_adjust_workload(workload, adjust_scale, base_load);
       });
     }
 
-    create_work(reads(my_workload), [=]{
-      my_value.get_reference() += do_workload(my_workload.get_value());
-    });
+    value += do_workload(workload);
 
   }
 
@@ -97,19 +78,28 @@ struct ImbalancedLoad {
 //==============================================================================
 
 
-void darma_main_task(std::vector<std::string> args) {
+int main(int argc, char** argv) {
 
-  std::size_t n_iterations_total = std::atoi(args.at(1).c_str());
-  std::size_t iterations_per_update = std::atoi(args.at(2).c_str());
-  std::size_t n_workers = std::atoi(args.at(3).c_str());
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  assert(argc == 4 or argc == 5);
+
+  std::size_t n_iterations_total = std::atoi(args[1]);
+  std::size_t iterations_per_update = std::atoi(args[2]);
+  std::size_t n_workers = std::atoi(args[3]);
 
   std::cout << "Running workload for " << n_iterations_total << " iterations, ";
   std::cout << "changing the workload every " << iterations_per_update
             << " iterations, for " << n_workers << " work indices." << std::endl;
 
+
   std::size_t random_seed;
-  if(args.size() == 5) {
-    random_seed = std::atoi(args.at(4).c_str());
+  if(argc == 5) {
+    random_seed = std::atoi(args[4]);
   }
   else {
     random_seed = 0;
@@ -121,28 +111,39 @@ void darma_main_task(std::vector<std::string> args) {
 
   std::size_t current_random_seed;
 
-  auto workloads = initial_access_collection<double>("workloads",
-    index_range=Range1D<std::size_t>(n_workers)
-  );
-  auto results = initial_access_collection<double>("results",
-    index_range=Range1D<std::size_t>(n_workers)
-  );
+  int my_offset = ((n_workers / size) * rank) + std::min(n_workers % size, rank);
+  int my_size = (n_workers / size) + int(rank < n_workers % size);
+
+  double workloads[my_size];
+  double results[my_size];
+
+  for(int i = 0; i < my_size; ++i) {
+    workloads[i] = results[i] = 0.0;
+  }
 
   for(std::size_t i = 0; i < n_iterations_total; ++i) {
 
     if(i % iterations_per_update == 0) {
-      if(i % iterations_per_update == 0) {
-        current_random_seed = seed_gen();
-      }
+      current_random_seed = seed_gen();
     }
 
-    create_concurrent_work<ImbalancedLoad>(
-      current_random_seed, i % iterations_per_update == 0,
-      workloads, results,
-      get_adjust_scale(i / iterations_per_update, n_iterations_total / iterations_per_update),
-      index_range = Range1D<std::size_t>(n_workers)
-    );
+    for(int my_idx = my_offset; my_idx < my_offset + my_size; ++my_idx) {
+
+      ImbalancedLoad()(
+        my_idx, n_workers,
+        current_random_seed, i % iterations_per_update == 0,
+        workloads[my_idx - my_offset], results[my_idx - my_offset],
+        get_adjust_scale(i / iterations_per_update, n_iterations_total / iterations_per_update)
+      );
+
+    }
+
   }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  MPI_Finalize();
+
 }
 
 DARMA_REGISTER_TOP_LEVEL_FUNCTION(darma_main_task);
