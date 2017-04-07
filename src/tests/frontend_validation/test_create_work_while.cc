a8e653412e27c910828b50c3882fdafa9538e555
David Hollman <dshollm@sandia.gov>
2017-04-06 17:30:15 -0700

    test_create_work_while works again

--- src/tests/frontend_validation/test_create_work_while.cc
+++ src/tests/frontend_validation/test_create_work_while.cc
@@ -292,7 +292,6 @@ TEST_F_WITH_PARAMS(TestCreateWorkWhile, basic_same_one_iteration,
 
 }
 
-#if 0
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -307,24 +306,31 @@ TEST_F(TestCreateWorkWhile, basic_different_always_false) {
     f_init, f_null,
     f_init_2, f_null_2, f_while_out_2
   );
+  use_t* init_use = nullptr;
+  use_t* do_init_use = nullptr;
   use_t* while_use = nullptr;
   use_t* do_sched_use = nullptr;
+  use_t* do_cont_use = nullptr;
 
   int value = 0;
 
-  EXPECT_INITIAL_ACCESS(f_init, f_null, make_key("hello"));
-  EXPECT_INITIAL_ACCESS(f_init_2, f_null_2, make_key("world"));
+  EXPECT_INITIAL_ACCESS(f_init, f_null, init_use, make_key("hello"));
+  EXPECT_INITIAL_ACCESS(f_init_2, f_null_2, do_init_use, make_key("world"));
 
   EXPECT_CALL(*mock_runtime, make_next_flow(f_init_2))
     .WillOnce(Return(f_while_out_2));
 
   EXPECT_REGISTER_USE_AND_SET_BUFFER(while_use, f_init, f_init, Read, Read, value);
   EXPECT_REGISTER_USE(do_sched_use, f_init_2, f_while_out_2, Modify, None);
+  EXPECT_REGISTER_USE(do_cont_use, f_while_out_2, f_null_2, Modify, None);
+  EXPECT_RELEASE_USE(do_init_use);
 
   EXPECT_REGISTER_TASK(while_use, do_sched_use);
 
   EXPECT_FLOW_ALIAS(f_init, f_null);
   EXPECT_FLOW_ALIAS(f_while_out_2, f_null_2);
+  EXPECT_RELEASE_USE(init_use);
+  EXPECT_RELEASE_USE(do_cont_use);
 
   //============================================================================
   // actual code being tested
@@ -355,6 +361,7 @@ TEST_F(TestCreateWorkWhile, basic_different_always_false) {
 
 }
 
+
 ////////////////////////////////////////////////////////////////////////////////
 
 TEST_F_WITH_PARAMS(TestCreateWorkWhile, basic_same_four_iterations,
@@ -373,24 +380,30 @@ TEST_F_WITH_PARAMS(TestCreateWorkWhile, basic_same_four_iterations,
   MockFlow f_do_out[] = { "f_do_out[0]", "f_do_out[1]", "f_do_out[2]", "f_do_out[3]",  "f_do_out[4]"};
   MockFlow f_while_fwd[] = { "f_while_fwd[0]", "f_while_fwd[1]", "f_while_fwd[2]", "f_while_fwd[3]",  "f_while_fwd[4]"};
   MockFlow f_while_out[] = { "f_while_out[0]", "f_while_out[1]", "f_while_out[2]", "f_while_out[3]",  "f_while_out[4]"};
+  use_t* init_use, *cont_use;
   use_t* do_use[] = { nullptr, nullptr, nullptr, nullptr };
+  use_t* do_cont_use[] = { nullptr, nullptr, nullptr, nullptr };
   use_t* while_use[] = { nullptr, nullptr, nullptr, nullptr, nullptr };
+  use_t* while_cont_use[] = { nullptr, nullptr, nullptr, nullptr, nullptr };
 
   int value = 0;
 
   bool while_as_functor = std::get<0>(GetParam());
   bool do_as_functor = std::get<1>(GetParam());
 
-  EXPECT_INITIAL_ACCESS(f_init, f_null, make_key("hello"));
+  EXPECT_INITIAL_ACCESS(f_init, f_null, init_use, make_key("hello"));
 
   EXPECT_CALL(*mock_runtime, make_next_flow(f_init))
     .WillOnce(Return(f_while_out[0]));
 
   EXPECT_REGISTER_USE_AND_SET_BUFFER(while_use[0], f_init, f_while_out[0], Modify, Read, value);
+  EXPECT_REGISTER_USE(cont_use, f_while_out[0], f_null, Modify, None);
+  EXPECT_RELEASE_USE(init_use);
 
   EXPECT_REGISTER_TASK(while_use[0]);
 
   EXPECT_FLOW_ALIAS(f_while_out[0], f_null);
+  EXPECT_RELEASE_USE(cont_use);
 
   //============================================================================
   // actual code being tested
@@ -449,6 +462,7 @@ TEST_F_WITH_PARAMS(TestCreateWorkWhile, basic_same_four_iterations,
     InSequence reg_before_release;
 
     EXPECT_REGISTER_USE_AND_SET_BUFFER(do_use[0], f_init, f_do_out[0], Modify, Modify, value);
+    EXPECT_REGISTER_USE(do_cont_use[0], f_do_out[0], f_while_out[0], Modify, None);
 
     EXPECT_RELEASE_USE(while_use[0]);
 
@@ -456,6 +470,7 @@ TEST_F_WITH_PARAMS(TestCreateWorkWhile, basic_same_four_iterations,
   }
 
   EXPECT_FLOW_ALIAS(f_do_out[0], f_while_out[0]);
+  EXPECT_RELEASE_USE(do_cont_use[0]);
 
   run_one_task(); // the first while
 
@@ -477,11 +492,15 @@ TEST_F_WITH_PARAMS(TestCreateWorkWhile, basic_same_four_iterations,
       EXPECT_REGISTER_USE_AND_SET_BUFFER(while_use[i+1], f_while_fwd[i], f_while_out[i+1],
         Modify, Read, value
       );
+      EXPECT_REGISTER_USE(while_cont_use[i+1], f_while_out[i+1], f_do_out[i],
+        Modify, None
+      );
 
       EXPECT_RELEASE_USE(do_use[i]);
 
       // TODO get rid of this and the corresponding make_next and just use existing out
       EXPECT_FLOW_ALIAS(f_while_out[i+1], f_do_out[i]);
+      EXPECT_RELEASE_USE(while_cont_use[i+1]);
 
       EXPECT_REGISTER_TASK(while_use[i+1]);
     }
@@ -504,11 +523,15 @@ TEST_F_WITH_PARAMS(TestCreateWorkWhile, basic_same_four_iterations,
         EXPECT_REGISTER_USE_AND_SET_BUFFER(do_use[i + 1],
           f_while_fwd[i], f_do_out[i + 1], Modify, Modify, value
         );
+        EXPECT_REGISTER_USE(do_cont_use[i + 1],
+          f_do_out[i + 1], f_while_out[i+1], Modify, None
+        );
 
         EXPECT_RELEASE_USE(while_use[i + 1]);
 
         // TODO get rid of this and the corresponding make_next and just use existing out
         EXPECT_FLOW_ALIAS(f_do_out[i+1], f_while_out[i+1]);
+        EXPECT_RELEASE_USE(do_cont_use[i+1]);
 
         EXPECT_REGISTER_TASK(do_use[i + 1]);
 
@@ -562,12 +585,17 @@ TEST_F(TestCreateWorkWhile, collection_one_iteration) {
   use_t* use_coll_cont = nullptr;
   use_t* coll_cont_use = nullptr;
   use_t* while_cont_use_2_c = nullptr;
+  use_t* while_cont_use_2 = nullptr;
+  use_t* init_use = nullptr;
+  use_t* init_use_c = nullptr;
+  use_t* cont_use = nullptr;
+  use_t* cont_use_c = nullptr;
 
   int value = 0;
   int values[4] = {0, 0, 0, 0};
 
-  EXPECT_INITIAL_ACCESS(f_init, f_null, make_key("hello"));
-  EXPECT_INITIAL_ACCESS_COLLECTION(f_init_c, f_null_c, make_key("world"));
+  EXPECT_INITIAL_ACCESS(f_init, f_null, init_use, make_key("hello"));
+  EXPECT_INITIAL_ACCESS_COLLECTION(f_init_c, f_null_c, init_use_c, make_key("world"), 4);
 
   EXPECT_CALL(*mock_runtime, make_next_flow(f_init))
     .WillOnce(Return(f_while_out));
@@ -576,11 +604,17 @@ TEST_F(TestCreateWorkWhile, collection_one_iteration) {
 
   EXPECT_REGISTER_USE_AND_SET_BUFFER(while_use, f_init, f_while_out, Modify, Read, value);
   EXPECT_REGISTER_USE_COLLECTION(while_use_c, f_init_c, f_while_out_c, Modify, None, 4);
+  EXPECT_REGISTER_USE(cont_use, f_while_out, f_null, Modify, None);
+  EXPECT_REGISTER_USE_COLLECTION(cont_use_c, f_while_out_c, f_null_c, Modify, None, 4);
+  EXPECT_RELEASE_USE(init_use);
+  EXPECT_RELEASE_USE(init_use_c);
 
   EXPECT_REGISTER_TASK(while_use, while_use_c);
 
   EXPECT_FLOW_ALIAS(f_while_out, f_null);
   EXPECT_FLOW_ALIAS(f_while_out_c, f_null_c);
+  EXPECT_RELEASE_USE(cont_use);
+  EXPECT_RELEASE_USE(cont_use_c);
 
   //============================================================================
   // actual code being tested
@@ -620,6 +654,7 @@ TEST_F(TestCreateWorkWhile, collection_one_iteration) {
   EXPECT_REGISTER_USE_COLLECTION(do_use_c, f_init_c, f_do_out_c, Modify, None, 4);
 
   EXPECT_REGISTER_USE_AND_SET_BUFFER(do_use, f_init, f_do_out, Modify, Modify, value);
+  EXPECT_REGISTER_USE(do_cont_use, f_do_out, f_while_out, Modify, None);
 
   {
     InSequence reg_before_release;
@@ -633,10 +668,11 @@ TEST_F(TestCreateWorkWhile, collection_one_iteration) {
 
   EXPECT_RELEASE_USE(while_use);
 
-  EXPECT_RELEASE_USE(do_cont_use_c);
-
   EXPECT_FLOW_ALIAS(f_do_out, f_while_out);
+  EXPECT_RELEASE_USE(do_cont_use);
+
   EXPECT_FLOW_ALIAS(f_do_out_c, f_while_out_c);
+  EXPECT_RELEASE_USE(do_cont_use_c);
 
   run_one_task(); // the first while
 
@@ -655,6 +691,8 @@ TEST_F(TestCreateWorkWhile, collection_one_iteration) {
   // The captured task collection use itself
   EXPECT_REGISTER_USE_COLLECTION(use_coll, f_init_c, f_out_coll, Modify, Modify, 4);
 
+  EXPECT_REGISTER_USE(while_cont_use_2, f_while_out_2, f_do_out, Modify, None);
+
   {
     InSequence reg_before_rel;
 
@@ -664,7 +702,7 @@ TEST_F(TestCreateWorkWhile, collection_one_iteration) {
     EXPECT_RELEASE_USE(do_use_c);
 
     EXPECT_CALL(*mock_runtime, register_task_collection_gmock_proxy(
-      UseInGetDependencies(ByRef(use_coll))
+      CollectionUseInGetDependencies(ByRef(use_coll))
     ));
   }
 
@@ -697,10 +735,13 @@ TEST_F(TestCreateWorkWhile, collection_one_iteration) {
   }
 
   EXPECT_RELEASE_USE(do_use);
-  EXPECT_RELEASE_USE(while_cont_use_2_c);
 
   EXPECT_FLOW_ALIAS(f_while_out_2, f_do_out);
+
+  EXPECT_RELEASE_USE(while_cont_use_2);
+
   EXPECT_FLOW_ALIAS(f_while_out_c_2, f_do_out_c);
+  EXPECT_RELEASE_USE(while_cont_use_2_c);
 
 
   run_one_task(); // the do part
@@ -766,20 +807,25 @@ TEST_F(TestCreateWorkWhile, functor_same_always_false) {
   DECLARE_MOCK_FLOWS(
     f_init, f_null, f_while_out
   );
+  use_t* init_use = nullptr;
   use_t* while_use = nullptr;
+  use_t* cont_use = nullptr;
 
   int value = 0;
 
-  EXPECT_INITIAL_ACCESS(f_init, f_null, make_key("hello"));
+  EXPECT_INITIAL_ACCESS(f_init, f_null, init_use, make_key("hello"));
 
   EXPECT_CALL(*mock_runtime, make_next_flow(f_init))
     .WillOnce(Return(f_while_out));
 
   EXPECT_REGISTER_USE_AND_SET_BUFFER(while_use, f_init, f_while_out, Modify, Read, value);
+  EXPECT_REGISTER_USE(cont_use, f_while_out, f_null, Modify, None);
+  EXPECT_RELEASE_USE(init_use);
 
   EXPECT_REGISTER_TASK(while_use);
 
   EXPECT_FLOW_ALIAS(f_while_out, f_null);
+  EXPECT_RELEASE_USE(cont_use);
 
   //============================================================================
   // actual code being tested
@@ -809,6 +855,4 @@ TEST_F(TestCreateWorkWhile, functor_same_always_false) {
 
   run_all_tasks();
 
-
 }
-#endif
