2220188b4bdd9775e50b44f53e09541fbbee1ddc
jjwilke <jjwilke@sandia.gov>
2018-02-04 09:55:54 -0800

    clean up license/readme everywhere

--- src/include/darma/interface/app/backend_hint.h
+++ src/include/darma/interface/app/backend_hint.h
@@ -6,7 +6,7 @@
 //                         DARMA
 //              Copyright (C) 2017 Sandia Corporation
 //
-// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// Under the terms of Contract DE-NA-0003525 with NTESS, LLC,
 // the U.S. Government retains certain rights in this software.
 //
 // Redistribution and use in source and binary forms, with or without
--- src/include/tinympl/swap_headers.py
+++ src/include/tinympl/swap_headers.py
@@ -14,7 +14,7 @@ new_header = '''/*
 //   Copyright (C) 2013, Ennio Barbaro.
 // See LEGAL.md for more information.
 //
-// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// Under the terms of Contract DE-NA-0003525 with NTESS, LLC,
 // the U.S. Government retains certain rights in this software.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -44,7 +44,7 @@ new_header = '''/*
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact David S. Hollman (dshollm@sandia.gov)
+// Questions? Contact darma@sandia.gov
 //
 // ************************************************************************
 //@HEADER
